c51f2ed49e84e4483c74e659c859f35bcbf13b90
kingmoc <joe.c.a.garcia@gmail.com>
2019-11-14 11:10:06 -0600

    Day 1 MVP stuff

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -12,22 +12,68 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        if value < self.value: #main case
+            if not self.left:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+
+        if value >= self.value: #main case #2
+            if not self.right:
+                self.right = BinarySearchTree(value) 
+            else:
+                self.right.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        print('ran function')
+        if target is self.value:
+            print('IS EQUAL', target, self.value)
+            return True
+
+        if target < self.value:
+            print("less Than")
+            if not self.left:
+                print('no left node')
+                return False
+            else:
+                return self.left.contains(target)
+
+        if target >= self.value:
+            print("greater Than")
+            if not self.right:
+                print('no right node')
+                return False
+            else:
+                return self.right.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
-
+        print('ran function')
+        if not self.right:
+            print('no right node', self.value)
+            return self.value
+        else:
+            print('right node exist')
+            return self.right.get_max()
+            
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
         pass
 
+# x = BinarySearchTree(5)
+# x.insert(30)
+# x.insert(3)
+# x.insert(7)
+# print(x.get_max())
+# print(x.contains(5))
+# print(x.contains(3))
+# print(x.contains(7))
+# print(x.contains(8))
+
+"""
     # DAY 2 Project -----------------------
 
     # Print all the values in order from low to high
@@ -55,3 +101,4 @@ class BinarySearchTree:
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
         pass
+"""
